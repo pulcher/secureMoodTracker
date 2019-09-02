d297b070f799f9f683a92578764e6d9e4d18cfb8
Harold Pulcher <pulcher@pulcher.biz>
2019-09-02 13:36:16 -0500

    building up the application. got the hw abstraction in place but need to add to it

--- secure-mt/secure-mt/main.c
+++ secure-mt/secure-mt/main.c
@@ -1,38 +1,30 @@
-﻿#include <stdbool.h>
+﻿#include <signal.h>
+#include <stdbool.h>
 #include <errno.h>
 #include <string.h>
 #include <time.h>
+#include <stdio.h>
+#include <stdarg.h>
 
-#include <applibs/log.h>
+#include "applibs_versions.h"
 #include <applibs/gpio.h>
+#include <applibs/log.h>
+#include <applibs/networking.h>
+#include <applibs/storage.h>
+
+//#include <hw/sample_hardware.h>
+
+// Azure IoT SDK
+#include <iothub_client_core_common.h>
+#include <iothub_device_client_ll.h>
+#include <iothub_client_options.h>
+#include <iothubtransportmqtt.h>
+#include <iothub.h>
+#include <azure_sphere_provisioning.h>
+
 
 int main(void)
 {
-    // This minimal Azure Sphere app repeatedly toggles GPIO 9, which is the green channel of RGB
-    // LED 1 on the MT3620 RDB.
-    // Use this app to test that device and SDK installation succeeded that you can build,
-    // deploy, and debug an app with Visual Studio, and that you can deploy an app over the air,
-    // per the instructions here: https://docs.microsoft.com/azure-sphere/quickstarts/qs-overview
-    //
-    // It is NOT recommended to use this as a starting point for developing apps; instead use
-    // the extensible samples here: https://github.com/Azure/azure-sphere-samples
-    Log_Debug(
-        "\nVisit https://github.com/Azure/azure-sphere-samples for extensible samples to use as a "
-        "starting point for full applications.\n");
-
-    int fd = GPIO_OpenAsOutput(9, GPIO_OutputMode_PushPull, GPIO_Value_High);
-    if (fd < 0) {
-        Log_Debug(
-            "Error opening GPIO: %s (%d). Check that app_manifest.json includes the GPIO used.\n",
-            strerror(errno), errno);
-        return -1;
-    }
-
-    const struct timespec sleepTime = {1, 0};
     while (true) {
-        GPIO_SetValue(fd, GPIO_Value_Low);
-        nanosleep(&sleepTime, NULL);
-        GPIO_SetValue(fd, GPIO_Value_High);
-        nanosleep(&sleepTime, NULL);
     }
 }
