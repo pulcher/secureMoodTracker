395bb14e0b8fbd2dea13137608ed0aeac7f04a18
Harold Pulcher <pulcher@pulcher.biz>
2019-09-02 18:30:25 -0500

    got data going to iot central again.   understand where I should put my button tracking code

--- secure-mt/secure-mt/main.c
+++ secure-mt/secure-mt/main.c
@@ -27,7 +27,18 @@
 
 static volatile sig_atomic_t terminationRequired = false;
 
-// Azure IoT Hub/Central variables
+// Azure IoT Hub/Central stuff
+#define SCOPEID_LENGTH 20
+static char scopeId[SCOPEID_LENGTH]; // ScopeId for the Azure IoT Central application, set in
+									 // app_manifest.json, CmdArgs
+static IOTHUB_DEVICE_CLIENT_LL_HANDLE iothubClientHandle = NULL;
+static bool iothubAuthenticated = false;
+static const int keepalivePeriodSeconds = 20;
+
+static void SendTelemetry(const unsigned char* key, const unsigned char* value);
+static void SendMessageCallback(IOTHUB_CLIENT_CONFIRMATION_RESULT result, void* context);
+static const char* GetReasonString(IOTHUB_CLIENT_CONNECTION_STATUS_REASON reason);
+static const char* getAzureSphereProvisioningResultString(AZURE_SPHERE_PROV_RETURN_VALUE provisioningResult);
 
 // Initialization/Cleanup
 static int InitPeripheralsAndHandlers(void);
@@ -43,16 +54,24 @@ static int buttonPollTimerFd = -1;
 static int azureTimerFd = -1;
 static int epollFd = -1;
 
+// Azure IoT poll periods
+static const int AzureIoTDefaultPollPeriodSeconds = 5;
+static const int AzureIoTMinReconnectPeriodSeconds = 5;
+static const int AzureIoTMaxReconnectPeriodSeconds = 1 * 60;
+
+static int azureIoTPollPeriodSeconds = -1;
+
 // Button state variables
 static GPIO_Value_Type sendMessageButtonState = GPIO_Value_High;
-static GPIO_Value_Type sendOrientationButtonState = GPIO_Value_High;
 
 static void ButtonPollTimerEventHandler(EventData* eventData);
 static bool IsButtonPressed(int fd, GPIO_Value_Type* oldState);
 static void SendMessageButtonHandler(void);
+static void AzureTimerEventHandler(EventData* eventData);
 
 // event handler data structures. Only the event handler field needs to be populated.
 static EventData buttonPollEventData = { .eventHandler = &ButtonPollTimerEventHandler };
+static EventData azureEventData = { .eventHandler = &AzureTimerEventHandler };
 
 /// <summary>
 ///     Signal handler for termination requests. This handler must be async-signal-safe.
@@ -63,9 +82,18 @@ static void TerminationHandler(int signalNumber)
 	terminationRequired = true;
 }
 
-int main(void)
+int main(int argc, char* argv[])
 {
 	Log_Debug("IoT Hub/Central Application starting.\n");
+
+	if (argc == 2) {
+		Log_Debug("Setting Azure Scope ID %s\n", argv[1]);
+		strncpy(scopeId, argv[1], SCOPEID_LENGTH);
+	}
+	else {
+		Log_Debug("ScopeId needs to be set in the app_manifest CmdArgs\n");
+		return -1;
+	}
 
 	// Fire up the timer and the Event Handlers
 	if (InitPeripheralsAndHandlers() != 0) {
@@ -78,6 +106,12 @@ int main(void)
 			terminationRequired = true;
 		}
 	}
+
+	//ClosePeripheralsAndHandlers();
+
+	Log_Debug("Application exiting.\n");
+
+	return 0;
 }
 
 /// <summary>
@@ -120,7 +154,160 @@ static int InitPeripheralsAndHandlers(void)
 		return -1;
 	}
 
+	azureIoTPollPeriodSeconds = AzureIoTDefaultPollPeriodSeconds;
+	struct timespec azureTelemetryPeriod = { azureIoTPollPeriodSeconds, 0 };
+	azureTimerFd =
+		CreateTimerFdAndAddToEpoll(epollFd, &azureTelemetryPeriod, &azureEventData, EPOLLIN);
+	if (buttonPollTimerFd < 0) {
+		return -1;
+	}
+
 	return 0;
+}
+
+/// <summary>
+///     Sets the IoT Hub authentication state for the app
+///     The SAS Token expires which will set the authentication state
+/// </summary>
+static void HubConnectionStatusCallback(IOTHUB_CLIENT_CONNECTION_STATUS result,
+	IOTHUB_CLIENT_CONNECTION_STATUS_REASON reason,
+	void* userContextCallback)
+{
+	iothubAuthenticated = (result == IOTHUB_CLIENT_CONNECTION_AUTHENTICATED);
+	Log_Debug("IoT Hub Authenticated: %s\n", GetReasonString(reason));
+}
+
+/// <summary>
+///     Sets up the Azure IoT Hub connection (creates the iothubClientHandle)
+///     When the SAS Token for a device expires the connection needs to be recreated
+///     which is why this is not simply a one time call.
+/// </summary>
+static void SetupAzureClient(void)
+{
+	if (iothubClientHandle != NULL)
+		IoTHubDeviceClient_LL_Destroy(iothubClientHandle);
+
+	AZURE_SPHERE_PROV_RETURN_VALUE provResult =
+		IoTHubDeviceClient_LL_CreateWithAzureSphereDeviceAuthProvisioning(scopeId, 10000,
+			&iothubClientHandle);
+	Log_Debug("IoTHubDeviceClient_LL_CreateWithAzureSphereDeviceAuthProvisioning returned '%s'.\n",
+		getAzureSphereProvisioningResultString(provResult));
+
+	if (provResult.result != AZURE_SPHERE_PROV_RESULT_OK) {
+
+		// If we fail to connect, reduce the polling frequency, starting at
+		// AzureIoTMinReconnectPeriodSeconds and with a backoff up to
+		// AzureIoTMaxReconnectPeriodSeconds
+		if (azureIoTPollPeriodSeconds == AzureIoTDefaultPollPeriodSeconds) {
+			azureIoTPollPeriodSeconds = AzureIoTMinReconnectPeriodSeconds;
+		}
+		else {
+			azureIoTPollPeriodSeconds *= 2;
+			if (azureIoTPollPeriodSeconds > AzureIoTMaxReconnectPeriodSeconds) {
+				azureIoTPollPeriodSeconds = AzureIoTMaxReconnectPeriodSeconds;
+			}
+		}
+
+		struct timespec azureTelemetryPeriod = { azureIoTPollPeriodSeconds, 0 };
+		SetTimerFdToPeriod(azureTimerFd, &azureTelemetryPeriod);
+
+		Log_Debug("ERROR: failure to create IoTHub Handle - will retry in %i seconds.\n",
+			azureIoTPollPeriodSeconds);
+		return;
+	}
+
+	// Successfully connected, so make sure the polling frequency is back to the default
+	azureIoTPollPeriodSeconds = AzureIoTDefaultPollPeriodSeconds;
+	struct timespec azureTelemetryPeriod = { azureIoTPollPeriodSeconds, 0 };
+	SetTimerFdToPeriod(azureTimerFd, &azureTelemetryPeriod);
+
+	iothubAuthenticated = true;
+
+	if (IoTHubDeviceClient_LL_SetOption(iothubClientHandle, OPTION_KEEP_ALIVE,
+		&keepalivePeriodSeconds) != IOTHUB_CLIENT_OK) {
+		Log_Debug("ERROR: failure setting option \"%s\"\n", OPTION_KEEP_ALIVE);
+		return;
+	}
+
+	//IoTHubDeviceClient_LL_SetDeviceTwinCallback(iothubClientHandle, TwinCallback, NULL);
+	IoTHubDeviceClient_LL_SetConnectionStatusCallback(iothubClientHandle,
+		HubConnectionStatusCallback, NULL);
+}
+
+/// <summary>
+///     Converts the IoT Hub connection status reason to a string.
+/// </summary>
+static const char* GetReasonString(IOTHUB_CLIENT_CONNECTION_STATUS_REASON reason)
+{
+	static char* reasonString = "unknown reason";
+	switch (reason) {
+	case IOTHUB_CLIENT_CONNECTION_EXPIRED_SAS_TOKEN:
+		reasonString = "IOTHUB_CLIENT_CONNECTION_EXPIRED_SAS_TOKEN";
+		break;
+	case IOTHUB_CLIENT_CONNECTION_DEVICE_DISABLED:
+		reasonString = "IOTHUB_CLIENT_CONNECTION_DEVICE_DISABLED";
+		break;
+	case IOTHUB_CLIENT_CONNECTION_BAD_CREDENTIAL:
+		reasonString = "IOTHUB_CLIENT_CONNECTION_BAD_CREDENTIAL";
+		break;
+	case IOTHUB_CLIENT_CONNECTION_RETRY_EXPIRED:
+		reasonString = "IOTHUB_CLIENT_CONNECTION_RETRY_EXPIRED";
+		break;
+	case IOTHUB_CLIENT_CONNECTION_NO_NETWORK:
+		reasonString = "IOTHUB_CLIENT_CONNECTION_NO_NETWORK";
+		break;
+	case IOTHUB_CLIENT_CONNECTION_COMMUNICATION_ERROR:
+		reasonString = "IOTHUB_CLIENT_CONNECTION_COMMUNICATION_ERROR";
+		break;
+	case IOTHUB_CLIENT_CONNECTION_OK:
+		reasonString = "IOTHUB_CLIENT_CONNECTION_OK";
+		break;
+	}
+	return reasonString;
+}
+
+/// <summary>
+///     Converts AZURE_SPHERE_PROV_RETURN_VALUE to a string.
+/// </summary>
+static const char* getAzureSphereProvisioningResultString(
+	AZURE_SPHERE_PROV_RETURN_VALUE provisioningResult)
+{
+	switch (provisioningResult.result) {
+	case AZURE_SPHERE_PROV_RESULT_OK:
+		return "AZURE_SPHERE_PROV_RESULT_OK";
+	case AZURE_SPHERE_PROV_RESULT_INVALID_PARAM:
+		return "AZURE_SPHERE_PROV_RESULT_INVALID_PARAM";
+	case AZURE_SPHERE_PROV_RESULT_NETWORK_NOT_READY:
+		return "AZURE_SPHERE_PROV_RESULT_NETWORK_NOT_READY";
+	case AZURE_SPHERE_PROV_RESULT_DEVICEAUTH_NOT_READY:
+		return "AZURE_SPHERE_PROV_RESULT_DEVICEAUTH_NOT_READY";
+	case AZURE_SPHERE_PROV_RESULT_PROV_DEVICE_ERROR:
+		return "AZURE_SPHERE_PROV_RESULT_PROV_DEVICE_ERROR";
+	case AZURE_SPHERE_PROV_RESULT_GENERIC_ERROR:
+		return "AZURE_SPHERE_PROV_RESULT_GENERIC_ERROR";
+	default:
+		return "UNKNOWN_RETURN_VALUE";
+	}
+}
+
+/// <summary>
+///     Close peripherals and handlers.
+/// </summary>
+static void ClosePeripheralsAndHandlers(void)
+{
+	Log_Debug("Closing file descriptors\n");
+
+	//// Leave the LEDs off
+	//if (deviceTwinStatusLedGpioFd >= 0) {
+	//	GPIO_SetValue(deviceTwinStatusLedGpioFd, GPIO_Value_High);
+	//}
+
+	CloseFdAndPrintError(buttonPollTimerFd, "ButtonTimer");
+	CloseFdAndPrintError(azureTimerFd, "AzureTimer");
+	//CloseFdAndPrintError(sendMessageButtonGpioFd, "SendMessageButton");
+	//CloseFdAndPrintError(sendOrientationButtonGpioFd, "SendOrientationButton");
+	//CloseFdAndPrintError(deviceTwinStatusLedGpioFd, "StatusLed");
+	CloseFdAndPrintError(epollFd, "Epoll");
 }
 
 /*
@@ -159,7 +346,7 @@ static void SendMessageButtonHandler(void)
 {
 	if (IsButtonPressed(sendMessageButtonGpioFd, &sendMessageButtonState)) {
 		Log_Debug("Button pressed\n");
-		//SendTelemetry("ButtonPress", "True");
+		SendTelemetry("ButtonPress", "True");
 	}
 }
 
@@ -174,5 +361,76 @@ static void ButtonPollTimerEventHandler(EventData* eventData)
 	}
 
 	SendMessageButtonHandler();
-	//SendOrientationButtonHandler();
+}
+
+/// <summary>
+/// Azure timer event:  Check connection status and send telemetry
+/// </summary>
+static void AzureTimerEventHandler(EventData* eventData)
+{
+	if (ConsumeTimerFdEvent(azureTimerFd) != 0) {
+		terminationRequired = true;
+		return;
+	}
+
+	bool isNetworkReady = false;
+	if (Networking_IsNetworkingReady(&isNetworkReady) != -1) {
+		if (isNetworkReady && !iothubAuthenticated) {
+			SetupAzureClient();
+		}
+	}
+	else {
+		Log_Debug("Failed to get Network state\n");
+	}
+
+	if (iothubAuthenticated) {
+		// this is where all the periodic status of things is sent.
+		//SendSimulatedTemperature();
+		IoTHubDeviceClient_LL_DoWork(iothubClientHandle);
+	}
+}
+
+// Azure stuff
+
+/// <summary>
+///     Sends telemetry to IoT Hub
+/// </summary>
+/// <param name="key">The telemetry item to update</param>
+/// <param name="value">new telemetry value</param>
+static void SendTelemetry(const unsigned char* key, const unsigned char* value)
+{
+	static char eventBuffer[100] = { 0 };
+	static const char* EventMsgTemplate = "{ \"%s\": \"%s\" }";
+	int len = snprintf(eventBuffer, sizeof(eventBuffer), EventMsgTemplate, key, value);
+	if (len < 0)
+		return;
+
+	Log_Debug("Sending IoT Hub Message: %s\n", eventBuffer);
+
+	IOTHUB_MESSAGE_HANDLE messageHandle = IoTHubMessage_CreateFromString(eventBuffer);
+
+	if (messageHandle == 0) {
+		Log_Debug("WARNING: unable to create a new IoTHubMessage\n");
+		return;
+	}
+
+	if (IoTHubDeviceClient_LL_SendEventAsync(iothubClientHandle, messageHandle, SendMessageCallback,
+		/*&callback_param*/ 0) != IOTHUB_CLIENT_OK) {
+		Log_Debug("WARNING: failed to hand over the message to IoTHubClient\n");
+	}
+	else {
+		Log_Debug("INFO: IoTHubClient accepted the message for delivery\n");
+	}
+
+	IoTHubMessage_Destroy(messageHandle);
+}
+
+/// <summary>
+///     Callback confirming message delivered to IoT Hub.
+/// </summary>
+/// <param name="result">Message delivery status</param>
+/// <param name="context">User specified context</param>
+static void SendMessageCallback(IOTHUB_CLIENT_CONFIRMATION_RESULT result, void* context)
+{
+	Log_Debug("INFO: Message received by IoT Hub. Result is: %d\n", result);
 }
